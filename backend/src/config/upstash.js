7dbfc9937cfad1d9ca9dfd27a0777732595d4dbb
David Enachescu <david.g.enachescu@gmail.com>
2025-07-31 01:22:30 +0300

    Adjusted rate limiting for deployment

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config();
 //5 requests per 10 sec
 const rateLimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, '10 s')
+    limiter: Ratelimit.slidingWindow(100, '30 s')
 })
 
 export default rateLimit;
