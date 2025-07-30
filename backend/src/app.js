73cb0658b86ab444124da02f242f94cef34cb227
David Enachescu <david.g.enachescu@gmail.com>
2025-07-31 01:01:34 +0300

    Prepared for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,20 +4,31 @@ import dotenv from 'dotenv';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectToDatabase } from './config/db.js';
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-}))
+if(process.env.NODE_ENV !== 'production'){
+  app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+  }))
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  });
+}
 
 connectToDatabase().then(() => {
   app.listen(PORT, () => {
